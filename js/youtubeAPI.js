45b8354f3fdf79759844c20ffe77ac65940cea03
awpas50 <awper9794@gmail.com>
2022-08-14 16:10:03 +1000

    Obtained the latest video publish time of a particular Youtube channel

--- js/youtubeAPI.js
+++ js/youtubeAPI.js
@@ -2,12 +2,12 @@ const APIKey = "AIzaSyAASD2cdA6_BLWREvJYxxVIY4u1nD9JJKg";
 const youtubeUser = "UC7fk0CB07ly8oSl0aqKkqFg";
 
 function UpdateCurrentTime() {
-    let currentTime = new Date().toISOString();
+    let currentTime = new Date().toISOString('YYYY-MM-DD');
     // call this function again in 500ms
     setTimeout(UpdateCurrentTime, 500);
     return currentTime;
 }
-function GetPreviousVideoTime() {
+function FetchChannelDetails() {
     return fetch(`https://www.googleapis.com/youtube/v3/search?key=${APIKey}&channelId=${youtubeUser}&part=snippet,id&order=date&maxResults=1`)
     .then(response => {
         if(response.ok) {
@@ -17,22 +17,16 @@ function GetPreviousVideoTime() {
             console.log('Fetching failed.');
         }
     })
-    .then(data => {
-        console.log(data);
-        let previousVideoTime = data['items'][0]['snippet'].publishedAt;
-        return previousVideoTime;
-    })
 }
-
-function CalculateTimeDifferences() {
-    let newPreviousVideoTime;
-    GetPreviousVideoTime().then((previousVideoTime) => {newPreviousVideoTime = previousVideoTime});
-    let timeDifferences = currentTime - newPreviousVideoTime;
-    console.log(timeDifferences);
+async function GetPreviousVideoTime(currentTime) {
+    const data = await FetchChannelDetails();
+    console.log(data);
+    let previousVideoTime = data['items'][0]['snippet'].publishedAt;
+    console.log(currentTime);
+    console.log(previousVideoTime);
+    let timeDifferences = currentTime - previousVideoTime;
     document.getElementById('timeDifferences').innerHTML = timeDifferences;
+    console.log(timeDifferences);
 }
-
 let currentTime = UpdateCurrentTime();
-console.log(currentTime);
-
-CalculateTimeDifferences();
+let channelData = GetPreviousVideoTime(currentTime);
