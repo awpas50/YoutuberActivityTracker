a215c81139392e85ea987cdc80f4914d2e2ff4e6
awpas50 <awper9794@gmail.com>
2022-08-27 23:14:22 +1000

    be able to calculate the time since preivous video

--- js/youtubeAPI.js
+++ js/youtubeAPI.js
@@ -1,8 +1,9 @@
 const APIKey = "AIzaSyDjyF2m9NimS6rc6OXNOPyDARtLuLBrD68";
 const youtubeUser = "UC7fk0CB07ly8oSl0aqKkqFg";
 
-function fetchChannelDetails() {
-    return fetch(`https://www.googleapis.com/youtube/v3/search?key=${APIKey}&channelId=${youtubeUser}&part=snippet,id&order=date&maxResults=1`)
+let fetchChannelData = new Promise((resolve, reject) => {
+    // FetchChannelDetails using Youtube Data API v3
+     fetch(`https://www.googleapis.com/youtube/v3/search?key=${APIKey}&channelId=${youtubeUser}&part=snippet,id&order=date&maxResults=1`)
     .then(response => {
         if(response.ok) {
             return response.json();
@@ -11,40 +12,47 @@ function fetchChannelDetails() {
             console.log('Fetching failed.');
         }
     })
-}
-async function getPreviousVideoTime() {
-    // FetchChannelDetails using Youtube Data API v3
-    const data = await fetchChannelDetails();
-    // Get previous video time.
-    let previousVideoTimeISOS = data['items'][0]['snippet'].publishedAt;
-    return Promise.resolve(previousVideoTimeISOS);
-}
-function updateCurrentTime() {
+    .then(data => {
+        // Get previous video time.
+        console.log(data);
+        let channelName = data['items'][0]['snippet'].channelTitle;
+        document.getElementById('secondText').textContent = "Time since " + channelName + "'s last video";
+        let previousVideoTimeISOS = data['items'][0]['snippet'].publishedAt;
+        console.log(previousVideoTimeISOS);
+        resolve(previousVideoTimeISOS);
+    })
+})
+let updateCurrentTime = () => {
     let currentTime = new Date();
     let currentTimeISOS = toIsoString(currentTime);
     return currentTimeISOS;
 }
-function toIsoString(date) {
+let calculateTimeDiff = (previousVideoTimeISOS, currentTimeISOS) => {
+    let timeDifferences = currentTimeISOS - previousVideoTimeISOS;
+    //console.log(currentTimeISOS);
+    //console.log(previousVideoTimeISOS);
+    console.log(getISOSTimeDiff(new Date(currentTimeISOS) - new Date(previousVideoTimeISOS)));
+}
+
+let toIsoString = (date) => {
     pad = function(num) {
         return (num < 10 ? '0' : '') + num;
     };
-
-return date.getFullYear() +
-    '-' + pad(date.getMonth() + 1) +
-    '-' + pad(date.getDate()) +
-    'T' + pad(date.getHours()) +
-    ':' + pad(date.getMinutes()) +
-    ':' + pad(date.getSeconds()) +
-    "Z";
+    return date.getFullYear() +
+        '-' + pad(date.getMonth() + 1) +
+        '-' + pad(date.getDate()) +
+        'T' + pad(date.getHours()) +
+        ':' + pad(date.getMinutes()) +
+        ':' + pad(date.getSeconds()) +
+        "Z";
 }
-
-var unitmapping = {"days":24*60*60*1000,
+             
+let getISOSTimeDiff = (diff) =>
+{
+    var unitmapping = {"days":24*60*60*1000,
                    "hours":60*60*1000,
                    "minutes":60*1000,
-                   "seconds":1000};
-                   
-function getISOSTimeDiff(diff)
-{
+                   "seconds":1000};     
     document.getElementById('days').textContent = Math.floor(diff/unitmapping.days);
     document.getElementById('hours').textContent = Math.floor((diff%unitmapping.days)/unitmapping.hours);
     document.getElementById('minutes').textContent = Math.floor((diff%unitmapping.hours)/unitmapping.minutes);
@@ -57,22 +65,12 @@ function getISOSTimeDiff(diff)
            Math.floor((diff%unitmapping.seconds))+" milliseconds";
 }
 
-function calculateTimeDiff(previousVideoTimeISOS) {
-    let currentTimeISOS = updateCurrentTime();
-    let timeDifferences = currentTimeISOS - previousVideoTimeISOS;
-    console.log(currentTimeISOS);
-    console.log(previousVideoTimeISOS);
-    console.log(getISOSTimeDiff(new Date(currentTimeISOS) - new Date(previousVideoTimeISOS)));
-    console.log(timeDifferences);
-    //setTimeout(calculateTimeDiff, 2000);
+function getTimeDiffEverySecond() {
+    fetchChannelData.then((previousVideoTimeISOS) => {
+        let currentTimeISOS = updateCurrentTime();
+        calculateTimeDiff(previousVideoTimeISOS, currentTimeISOS);
+    });
+    setTimeout(getTimeDiffEverySecond, 1000);
 }
 
-async function main() {
-    //let previousVideoTimeISOS = await getPreviousVideoTime();
-    let previousVideoTime = new Date(2022, 5, 13, 23, 33, 30);
-    let previousVideoTimeISOS = toIsoString(previousVideoTime);
-    calculateTimeDiff(previousVideoTimeISOS);
-    setTimeout(main, 1000);
-}
-
-main();
+getTimeDiffEverySecond();
